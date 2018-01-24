517ab037cb23ca49e3b7bc741e3e0c41ae7f6761
Shaun Hutch <shaunoff@Shauns-MBP.home>
2018-01-24 16:32:48 -0500

    formatting

--- src/CanvasTest.js
+++ src/CanvasTest.js
@@ -1,43 +1,75 @@
 import React, { Component } from 'react';
-
+import {
+	MdFormatShapes,
+	MdFormatSize,
+	MdInsertPhoto,
+	MdCropSquare,
+	MdFormatColorFill,
+	MdFormatColorText,
+	MdFormatBold,
+	MdFormatItalic,
+	MdFormatUnderlined,
+} from 'react-icons/lib/md';
 
 
 class CanvasTest extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			text: false
+			text: false,
+			textPositionSelect: false
 		}
 		this.addText = this.addText.bind(this)
 		this.changeColor = this.changeColor.bind(this)
+		this.textPosition = this.textPosition.bind(this)
 	}
 
 	componentDidMount(){
 		this.canvas = new window.fabric.Canvas('fabricTest');
 		console.log(this.canvas)
+		this.canvas.on('mouse:down', (click) =>{
+  		if(this.state.textPositionSelect){
+				this.addText(click.e.layerX, click.e.layerY)
+				this.setState({textPositionSelect: false})
+			}
+		})
 	}
-	addText(){
-		var text = new window.fabric.IText('gdfhjdg', { left: 100, top: 100 , opacity: 0.4});
-		text.on('selected', () => {
- 			this.setState({text: true})
-		});
+	addText(x,y){
+		var text = new window.fabric.IText('Text Box', { left: x, top: y , opacity: 1});
 		this.canvas.add(text);
+	}
+	textPosition(){
+		this.setState({textPositionSelect: !this.state.textPositionselect})
 	}
 	changeColor(){
 		const obj = this.canvas.getActiveObject()
-		console.log(obj)
-		obj.setColor("#6bada7")
-		obj.opacity = 0.1
+		var style = { };
+    style.fill = '#007681';
+		obj.setSelectionStyles(style)
+		// obj.setColor("#6bada7")
+		// obj.opacity = 0.1
 		this.canvas.renderAll()
-
 	}
 	render() {
 
 		return (
 			<div>
-				{this.state.text && <div onClick={this.changeColor}>change to red</div>}
-				<div onClick={this.addText}>This is a test canvas</div>
-				<canvas id="fabricTest" width="500" height="500" style={{border: '2px solid #ccc'}}/>
+				<div onClick={this.changeColor}>change to red</div>
+				<div>This is a test canvas</div>
+				<div onClick={this.textPosition}>Text Active</div>
+				<div style={{display: 'flex', background: "#fafafa", border: '1px solid #ccc', width: '500px', marginLeft: '60px'}}>
+					<MdFormatShapes onClick={this.textPosition} style={{margin: "10px"}} size="25" color="rgba(0,0,0,.5)"/>
+					<MdInsertPhoto style={{margin: "10px"}} size="25" color="rgba(0,0,0,.5)"/>
+					<MdCropSquare style={{margin: "10px"}} size="25" color="rgba(0,0,0,.5)"/>
+					<div style={{borderRight: '1px solid #ccc'}}></div>
+					<MdFormatColorText style={{margin: "10px"}} size="25" color="rgba(0,0,0,.5)"/>
+					<MdFormatColorFill style={{margin: "10px"}} size="25" color="rgba(0,0,0,.5)"/>
+					<div style={{borderRight: '1px solid #ccc'}}></div>
+					<MdFormatBold style={{margin: "10px"}} size="25" color="rgba(0,0,0,.5)"/>
+					<MdFormatItalic style={{margin: "10px"}} size="25" color="rgba(0,0,0,.5)"/>
+					<MdFormatUnderlined style={{margin: "10px"}} size="25" color="rgba(0,0,0,.5)"/>
+				</div>
+				<canvas id="fabricTest" width="500" height="500" style={{marginLeft: "60px", border: '1px solid #ccc'}}/>
 			</div>
 		);
 	}
